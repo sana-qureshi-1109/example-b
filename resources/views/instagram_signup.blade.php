c869439c77989189efbbe89118b5efc005745bd5
Sana Qureshi <qureshisana7861@gmail.com>
2024-10-14 20:44:10 +0530

    code remove

--- resources/views/instagram_signup.blade.php
+++ resources/views/instagram_signup.blade.php
@@ -8,21 +8,14 @@
 <body>
     <center>
     <h1><b>Instagram</b></h1>
-    <h2>S</h2>
-    <h2>Qureshi</h2>
     <h4>Sign up to see photos and videos <br>
           from your friends</h4>
           <button>Log in with facebook</button>
  <h4>OR</h4>
- <form>
-
  <input type="email" id="mobile_email" name="mobile_email" placeholder="Mobile Number orEmail"required><br>
  <br><input type="password" id="psw" name="psw" placeholder="Password" required><br>
  <br><input type="text" id="f_name" name="f_name" placeholder="Full Name"required><br>
  <br><input type="text" id="u_name" name="u_name" placeholder="Username Name"><br>
- <br><input type="radio" id="gender" name="gender">Male<br>
- <br><input type="radio" id="gender" name="gender">Female<br>
- 
 <h5>People who use our service may have uploaded<br>
 your contact information to Instagram. Learn more</h5>
 <h5>By signing up, you agree to our Terms, Privacy<br>
